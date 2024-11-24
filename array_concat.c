a41bd3a05e645eb8a8b4f9d82cdf536ceb1545dd
Prabhu <prabhuprathamesh444@gmail.com>
2024-11-24 21:05:06 +0530

    question: 1929

---
+++ array_concat.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* getConcatenation(int* nums, int numsSize, int* returnSize) {
+    int* ans = (int*)malloc(2 * numsSize * sizeof(int));
+    
+    for(int i = 0;i< numsSize; i++)
+    {
+        ans[i] = nums[i];
+        ans[i + numsSize] = nums[i];
+    }
+    *returnSize = 2*numsSize;
+    return ans;
+}
+
+int main() {
+    int nums[] = {1, 2, 3,1};
+    int numsSize = sizeof(nums) / sizeof(nums[0]);
+    int returnSize;
+
+    int* result = getConcatenation(nums, numsSize, &returnSize);
+
+    for (int i = 0; i < returnSize; i++)
+            printf("%d ", result[i]);
+
+    free(result);
+}
