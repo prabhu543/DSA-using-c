11ddfd0a357ce758db6be7ad892411b27f3c7086
Prabhu <prabhuprathamesh444@gmail.com>
2024-11-27 04:59:45 +0530

    question: 2520

---
+++ DigitDivision.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+int countDigits(int num) {
+    int count = 0;
+    int n = num;
+    while(num > 0)
+    {
+        int rem = num % 10;
+        if( n % rem == 0 )
+        {
+            count ++;
+        }
+        num = num / 10;
+    }
+    return count;
+}
+
+int main() {
+    int number = 1248;
+    int result = countDigits(number);
+    printf("Result: %d\n", result);
+    return 0;
+}
